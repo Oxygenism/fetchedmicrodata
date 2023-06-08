907e14ad941dec38996985e9490bd762ab814ec3
Oxygenism <oxygenism@horchner.dev>
2023-06-08 11:36:22 +0200

    Remove prop container

--- script.js
+++ script.js
@@ -1,43 +1,49 @@
 const samplePropData = [
     {
-        "type": "itemProp",
-        "prop": "name",
-        "content": "test-name"
-    },
-    {
-        "type": "itemProp",
-        "prop": "sku",
-        "content": "test-sku"
-    },
-    {
-        "type": "itemProp",
-        "prop": "brand",
-        "content": "test-brand"
-    },
-    {
-        "type": "itemProp",
-        "prop": "image",
-        "content": "test-image"
-    },
-    {
         "type": "itemScope",
-        "schema": "https://schema.org/Offer",
-        "prop": "offers",
+        "schema": "https://schema.org/Product",
         "items": [
             {
                 "type": "itemProp",
-                "prop": "offercount",
-                "content": "3",
+                "prop": "name",
+                "content": "test-name"
             },
             {
                 "type": "itemProp",
-                "prop": "lowPrice",
-                "content": "11.11"
+                "prop": "sku",
+                "content": "test-sku"
             },
             {
                 "type": "itemProp",
-                "prop": "highPrice",
-                "content": "22.22"
+                "prop": "brand",
+                "content": "test-brand"
+            },
+            {
+                "type": "itemProp",
+                "prop": "image",
+                "content": "test-image"
+            },
+            {
+                "type": "itemScope",
+                "schema": "https://schema.org/Offer",
+                "prop": "offers",
+                "items": [
+                    {
+                        "type": "itemProp",
+                        "prop": "offercount",
+                        "content": "3",
+                    },
+                    {
+                        "type": "itemProp",
+                        "prop": "lowPrice",
+                        "content": "11.11"
+                    },
+                    {
+                        "type": "itemProp",
+                        "prop": "highPrice",
+                        "content": "22.22"
+                    }
+                ]
             }
         ]
     }
@@ -52,7 +58,10 @@ function generate(container, properties) {
             container.appendChild(meta);
         } else if (property.type === 'itemScope') {
             var div = document.createElement("div");
-            div.setAttribute('itemProp', property.prop);
+            if (property.prop) {
+                div.setAttribute('itemProp', property.prop);
+            }
+
             div.setAttribute('itemScope', '');
             div.setAttribute('itemType', property.schema);
 
@@ -63,5 +72,5 @@ function generate(container, properties) {
     }
 }
 
-let propContainer = document.getElementById("prop-container");
+let propContainer = document.getElementById("container");
 generate(propContainer, samplePropData);
